e30af861ce55f01966521d2ebc2180c3757cf030
Tomas Gons <tomasgons@gmail.com>
2020-09-19 19:06:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const keys = document.querySelectorAll(".key");
 const playSound = e => {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
@@ -10,6 +11,7 @@ function removeTransition(e) {
   if (e.propertyName !== "transform") return;
   e.target.classList.remove("playing");
 }
-const keys = document.querySelectorAll(".key");
+
 keys.forEach(key => key.addEventListener("transitionend", removeTransition));
+
 window.addEventListener("keydown", playSound);
